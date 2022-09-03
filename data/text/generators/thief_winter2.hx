8fcd87dc8e06992659073981ad828618250134ef
godisgoodmatt <66806614+godisgoodmatt@users.noreply.github.com>
2022-09-03 00:23:44 -0400

    removed old thief items from thief 2 gen

--- data/text/generators/thief_winter2.hx
+++ data/text/generators/thief_winter2.hx
@@ -1,5 +1,5 @@
 var jestershops = shuffle(["Mouse Trap", "Doom and Gloom", "Pip Master", "Christmas Pistol","Blighted Zap", "Christmas Revolver", "Nine Iron", "Shrug", "Gatling Pea", "Brace", "Pocket Protector", "Electric Arc", "Rock Crusher", "Dime", "Bloody Drill", "Power Glove", "Frosty Bufu", "Dynamite Shotgun", "Sandbag", "Asklepios", "Matchbook"]);
-var strangeshop = shuffle(["Booby Trap", "Bet", "Wide Spatula", "Ice Skates", "Quake Sigma", "Sickle", "Dazzler", "Cactus Cauldron"]);
+var strangeshop = shuffle(["Wide Spatula", "Ice Skates", "Quake Sigma", "Sickle", "Dazzler", "Cactus Cauldron"]);
 var awesomelist = shuffle(["Tennis Racket", "Doom and Gloom", "Christmas Pistol","Countvalanche", "Roulette", "AC Unit", "Christmas Revolver", "Flame Lance", "Poison Sting", "Power Glove", "Ukulele", "Shovel Blade", "Lucky Punch"]);
 var floor2gooditem = shuffle(["Budge", "Pip Master"]);
 var floor3item = shuffle(["Parry", "Double Parry", "Lightreaver", "Minigun", "Shielding Potion", "The Monarch", "Innovate"]);
