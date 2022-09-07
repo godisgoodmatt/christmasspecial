aa8568cab8f0fbd80edc832e537bd173b98cecfe
That-One-Dude <tony.skeps@gmail.com>
2022-09-06 20:10:22 -0500

    Changed Mariah Carey's episode
    now you only gain a note on odd turns
    Now your deck shuffles on every odd turn

--- data/text/scripts/christmasspecial/mariahcarey/todeckmode.hx
+++ data/text/scripts/christmasspecial/mariahcarey/todeckmode.hx
@@ -1,9 +1,12 @@
 var self = args[0];
 
-self.changetodecklayout(self.getvar("cards").split(","));
+self.changetodecklayout(shuffle(self.getvar("cards").split(",")));
 
-var skillcard = self.createskillcard(self.getvar("skillcard"), []);
+var skillcard = self.createskillcard("Jester Deck", []);
 self.equipment.push(skillcard);
+
+self.setvar("equipmentstash", "");
+self.setvar("equipmentupgraded", "");
 
 // This just gives the card outlines
 Deck.sethandsize(3,true);
