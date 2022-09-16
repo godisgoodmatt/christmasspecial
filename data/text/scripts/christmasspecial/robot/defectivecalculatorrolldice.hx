9f6ec7bd0d5bdbc0e501a70c5402c91481b48506
That-One-Dude <tony.skeps@gmail.com>
2022-09-15 21:51:46 -0500

    Now Defective Calculator doesn't error out when rolling all of your dice

--- data/text/scripts/christmasspecial/robot/defectivecalculatorrolldice.hx
+++ data/text/scripts/christmasspecial/robot/defectivecalculatorrolldice.hx
@@ -2,27 +2,23 @@
 var self = args[0];
 var e = args[1];
 
-var rolls = rand(self.getvar("rolls"));
-givedice([rolls]);
-runscript("christmasspecial/mqspecialdice",[self,target,"onstartturn_player",turn]);
+if (self.getvar("rollhistory").length < self.getvar("numrolls")) {
 
-self.getvar("rollhistory").push(rolls);
+	var rolls = rand(self.getvar("rolls"));
+	givedice([rolls]);
+	runscript("christmasspecial/mqspecialdice",[self,target,"onstartturn_player",turn]);
 
-if(chance(self.getvar("errorchance"))){
-	inflictself("ncr_error");
-	sfx("_curse");
-	self.getvar("survivalhistory").push(false);
-} else {
-	self.getvar("survivalhistory").push(true);
-}
+	self.getvar("rollhistory").push(rolls);
 
-if (self.getvar("rollhistory").length >= self.getvar("numrolls")) {
-	e.animate("error");
-	sfx("roboterror");
-	e.ready = false;
-	e.active = false;
-	e.show = false;
-	for (display in e.getvar("displays")) {
-		display.show = false;
+	if(chance(self.getvar("errorchance"))){
+		inflictself("ncr_error");
+		sfx("_curse");
+		self.getvar("survivalhistory").push(false);
+	} else {
+		self.getvar("survivalhistory").push(true);
+	}
+
+	if (self.getvar("rollhistory").length == self.getvar("numrolls")) {
+		sfx("roboterror");
 	}
 }
