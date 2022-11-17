df2c58a4c5f38fce4a5351d6011bab26c5c369ef
EdX7 <edluo1@bu.edu>
2022-11-17 02:00:18 -0800

    Added new dungeon chest/shop layout for Inventor

--- data/text/generators/inventor_maintenance.hx
+++ data/text/generators/inventor_maintenance.hx
@@ -4,10 +4,10 @@ var floor1countdowns = ["Eggnog", "Sceptre", "Chain Knife", "Steamstaff", "Revol
 var jackpothelpers = ["Chaos Engineering", "Concatenate", "Increment", "Cooling Fan", "Fixed Payout"];
 var floor1crap = ["Flicker","Pocket Sand","Washcloth","Create Opening","Chain Link","Ubernudge","Zoop Zoop","Loud Brostep", "Crazy Glue"];
 var commonlist1 = ["Tennis Racket", "Umbrella", "Sharpie", "Bronze Medal", "Stockpick", "Waltzer", "Safety Scissors", "Natural Charm", "Glue Pact", "Plasma Rifle"];
-var shoplist1 = ["Tennis Racket", "Scorpion", "Technology", "Rising Dagger", "Compact Resonator", "Burst Beam", "Repast Rapier", "Hand Sanitiser", "Spin Attack", pick(["Eternal Cauldron","Plug Socket"]), "Metal Crusher", "Iron Helmet", "Two Handed Spatula", "Contact Juggling"];
+var shoplist1 = ["Tennis Racket", "Technology", "Rising Dagger", "Compact Resonator", "Burst Beam", "Repast Rapier", "Hand Sanitiser", "Spin Attack", pick(["Eternal Cauldron","Plug Socket"]), "Metal Crusher", "Iron Helmet", "Two Handed Spatula", "Contact Juggling"];
 if (chance(30)) shoplist1.push("Gavel");
 if (chance(30)) shoplist1.push("Sledgehammer");
-var midtierdrops = ["Goggles", "Permapick", "Boston Basher", "Matchbook", "Technology", "God's Pick", pick(["Wispy Kaboom Smash", "Switchblade"]), "Tootsie Pop"];
+var midtierdrops = ["Goggles", "Permapick", "Scorpion", "Boston Basher", "Matchbook", "Technology", "God's Pick", pick(["Wispy Kaboom Smash", "Switchblade"]), "Tootsie Pop"];
 var floor3items = ["Vacuum", "Occam's Razor", "Dice Pump", "Adamant Rail", pick(["Hall of Daggers", "Devilsknife"]), "Stop Sign", "Popcorn", "Gatling Pea", pick(["Reverse Emulation","Supersonic"]), "Ice Skates", "Zeus Dagger"];
 var commonlist2 = ["Return Bicep Curls", "Sneksword", "Ale Bat", "Waltzer", "The Monarch", "Janky Spanner", "Spirit Shield", "Present Shotgun", "Incapacitate", "Ubersaw", "Storm Reactor", "Stygian Blade", "Sand Hawk"];
 commonlist2.push(pick(jackpothelpers));
@@ -66,32 +66,28 @@ mycoolfloor1.theme = rand([music.pop()]);
 mycoolfloor1.generate();
   
 //Floor 2:
-items = [];
-gooditems = [];
+items = [jackpothelpers.pop()];
+gooditems = [commonlist1.pop()];
 
 otherstuff = [
   health(), 
   health()
 ];
 goodotherstuff = [
-  shop(shuffle([commonlist1.pop(),commonlist1.pop(),commonlist1.pop()]), shuffle([1, 2, 3])),
   shop(shuffle([shoplist1.pop(), shoplist1.pop(), pick(jackpothelpers)]), shuffle([2, 1, 1])),
-  shop(shuffle([shoplist1.pop(),shoplist1.pop(),shoplist1.pop()]), shuffle([2, 2, 1])),
-  shop(shuffle(["upgrade", shoplist1.pop(), pick(jackpothelpers)]), shuffle([2, 1, 2])),
-  upgrade()
+  shop(shuffle(["upgrade", shoplist1.pop(), pick(jackpothelpers)]), shuffle([2, 1, 2]))
 ];
 var mycoolfloor2 = addfloor('inventorlarge').additems(items, gooditems).addotherstuff(otherstuff, goodotherstuff);
 mycoolfloor2.theme = rand([music.pop()]);
 mycoolfloor2.generate();
 
 //Floor 3:
-items = [commonlist2.pop(),commonlist2.pop()];
-gooditems = [];
+items = [commonlist2.pop()];
+gooditems = [commonlist2.pop()];
 
 otherstuff = [health()];
 goodotherstuff = [
   shop(shuffle([jackpothelpers.pop(),floor3items.pop(),floor3items.pop()]), shuffle([1, 2, 1]))
-  shop(shuffle([commonlist2.pop(), commonlist2.pop(), commonlist2.pop()])),
   shop(shuffle([midtierdrops.pop(),"health",midtierdrops.pop()]), shuffle([1, 2, 1])),
   upgrade(),
   upgrade()
@@ -101,29 +97,26 @@ mycoolfloor3.theme = rand([music.pop()]);
 mycoolfloor3.generate();
   
 //Floor 4:
-items = [];
-gooditems = [];
+items = [floor1crap.pop()];
+gooditems = [midtierdrops.pop()];
 		
 otherstuff = [health(), health()];
 goodotherstuff = [
-  shop(shuffle([floor1crap.pop(),shoplist1.pop(),floor1crap.pop()]), shuffle([3, 2, 1])),
-  shop(shuffle([floor1crap.pop(),midtierdrops.pop(),floor1crap.pop()]), shuffle([1, 2, 1])),
-  shop(shuffle([floor1crap.pop(),commonlist1.pop(),commonlist2.pop()]), shuffle([3, 2, 4]))
+  shop(shuffle([shoplist1.pop(),shoplist1.pop(),floor3items.pop()]), shuffle([3, 2, 1])),
+  shop(shuffle([floor1crap.pop(),commonlist1.pop(),commonlist2.pop()]), shuffle([3, 1, 4]))
 ];
 var mycoolfloor4 = addfloor('inventorlarge').additems(items, gooditems).addotherstuff(otherstuff, goodotherstuff);
 mycoolfloor4.theme = rand([music.pop()]);
 mycoolfloor4.generate();
   
 //Floor 5:
-items = [];
-gooditems = [];
+items = [midtierdrops.pop()];
+gooditems = [floor5items.pop()];
 		
 otherstuff = [health(), health()];
 goodotherstuff = [
-  shop(shuffle([floor5items.pop(),floor5items.pop(),midtierdrops.pop()]), shuffle([2, 2, 1])),
-  shop(shuffle([midtierdrops.pop(),midtierdrops.pop(),commonlist1.pop()]), shuffle([1, 2, 1])),
-  shop(shuffle([commonlist2.pop(), "health", commonlist2.pop()])),
-  shop(shuffle([shoplist1.pop(),commonlist1.pop(),shoplist1.pop()]), shuffle([2, 2, 1]))
+  shop(shuffle([floor5items.pop(), "health",midtierdrops.pop()])),
+  shop(shuffle([midtierdrops.pop(),midtierdrops.pop(),commonlist1.pop()]), shuffle([1, 2, 1]))
 ];
 		
 var mycoolfloor5 = addfloor('inventorlarge').additems(items, gooditems).addotherstuff(otherstuff, goodotherstuff);
@@ -134,7 +127,11 @@ mycoolfloor5.generate();
 items = [];
 gooditems = [];
 otherstuff = [];
-goodotherstuff = [shop(["upgrade","BOOO!","BOOO!"]), shuffle([2, 2, 1])];   
+goodotherstuff = [];
+
+if (getfinalboss() == "Mariah Carey"){
+  items.push("BOOO!");
+}
 
 var mycoolfloor6 = addfloor('boss').additems(items, gooditems).addotherstuff(otherstuff, goodotherstuff);
 mycoolfloor6.theme = rand(['bossy']);
