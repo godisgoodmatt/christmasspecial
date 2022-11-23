5b8748c4f278d492a24c834b867f2a23025d768b
That-One-Dude <tony.skeps@gmail.com>
2022-11-23 10:24:27 -0600

    Changed the genereator to give a more curated set of items

--- data/text/generators/warrior_winter3.hx
+++ data/text/generators/warrior_winter3.hx
@@ -1,6 +1,57 @@
-var awesomelist = shuffle(["Bump", "Bump", "Nudge", "Nudge", "Spatula", "Spatula", "Spanner", "Spanner", "Lockpick", "Lockpick", "Doppeldice", "Doppeldice", "Duplicate", "Duplicate", "Table Slam", "Chocolate Cookie", "Hookshot", "Snake Eye Charm", "Square Pair", "Midnight Charm", "Half Moon Charm", "Counterfeit", "Hacksaw", "Crowbar", "Chisel", "Sine Wave", "Saw Wave", "Berlin Key", "Master Key", "Skeleton Key", "Blender", "Magic Key", "Woof Woof Woof", "Cauldron", "Spare 1", "Spare 2","Spare 3","Spare 4","Spare 5","Spare 6", "Illuminate", "Paper Lantern", "Picnic Basket", "Marmalade", "Befuddle", "Signal Jammer", "Cauldron", "Splitula", "Bronze Cauldron", "Mirror Cauldron", "Glass Cauldron", "Silver Cauldron", "Gold Cauldron", "Blood Cauldron", "Crucible", "Broken Mirror", "Hall of Mirrors", "Honey", "Lucky Roll", "Avalanche", "Smush Together", "Change Machine", "Rat", "Parity Flip", "Rerow", "Scrambler", "Obsidian Bauble", "Call for Backup Robot?", "Lollipop", "Subdivide", "Lucky Star", "Fury Spell", "Doppeltwice", "Budge", "Pip Master", "Disco Ball", "Giant Spatula", "Bearpick", "Magic Reroll", "Ungeradedice", "Candle", "Radicalise", "5patula", "Waterwheel", "Logistics Expert", "Half Moon Cauldron", "One I Prepared Earlier", "Dullplicate", "Bumper Sticker", "Murk", "Trinity Charm", "Luckpick", "Buzzsaw", "Eternal Cauldron", "Contact Juggling", "Dime", "Burner Phone", "Demote", "Rockfall", "Locktrick", "Mitosis", "Janky Spanner", "Metal Crusher", "Plug Socket", "Natural Charm", "Mass Grimoire", "Rock Crusher", "Shrug", "Star Cauldron", "Mirror Spatula", "Halfula", "Endless Pond", "Chain Link", "God's Pick", "Dice Pump", "Sharpie", "Transporter", "Fogged Mirror", "Decantation", "Caustic Cauldron", "Cactus Cauldron", "Wide Spatula", "Deva Form", "Permapick", "Vice Grip"]);
-var strangeshop = shuffle(["Backup Plan", "Pocket Rock"]);
+/*var awesomelist = shuffle(["Bump", "Bump", "Nudge", "Nudge", "Spatula", "Spatula", "Spanner", "Spanner", "Lockpick", "Lockpick", "Doppeldice", "Doppeldice", "Duplicate", "Duplicate", "Table Slam", "Chocolate Cookie", "Hookshot", "Snake Eye Charm", "Square Pair", "Midnight Charm", "Half Moon Charm", "Counterfeit", "Hacksaw", "Crowbar", "Chisel", "Sine Wave", "Saw Wave", "Berlin Key", "Master Key", "Skeleton Key", "Blender", "Magic Key", "Woof Woof Woof", "Cauldron", "Spare 1", "Spare 2","Spare 3","Spare 4","Spare 5","Spare 6", "Illuminate", "Paper Lantern", "Picnic Basket", "Marmalade", "Befuddle", "Signal Jammer", "Cauldron", "Splitula", "Bronze Cauldron", "Mirror Cauldron", "Glass Cauldron", "Silver Cauldron", "Gold Cauldron", "Blood Cauldron", "Crucible", "Broken Mirror", "Hall of Mirrors", "Honey", "Lucky Roll", "Avalanche", "Smush Together", "Change Machine", "Rat", "Parity Flip", "Rerow", "Scrambler", "Obsidian Bauble", "Call for Backup Robot?", "Lollipop", "Subdivide", "Lucky Star", "Fury Spell", "Doppeltwice", "Budge", "Pip Master", "Disco Ball", "Giant Spatula", "Bearpick", "Magic Reroll", "Ungeradedice", "Candle", "Radicalise", "5patula", "Waterwheel", "Logistics Expert", "Half Moon Cauldron", "One I Prepared Earlier", "Dullplicate", "Bumper Sticker", "Murk", "Trinity Charm", "Luckpick", "Buzzsaw", "Eternal Cauldron", "Contact Juggling", "Dime", "Burner Phone", "Demote", "Rockfall", "Locktrick", "Mitosis", "Janky Spanner", "Metal Crusher", "Plug Socket", "Natural Charm", "Mass Grimoire", "Rock Crusher", "Shrug", "Star Cauldron", "Mirror Spatula", "Halfula", "Endless Pond", "Chain Link", "God's Pick", "Dice Pump", "Sharpie", "Transporter", "Fogged Mirror", "Decantation", "Caustic Cauldron", "Cactus Cauldron", "Wide Spatula", "Deva Form", "Permapick", "Vice Grip", "Countvalanche"]);*/
 var vampireitem = ["Silver Sword"];
+
+var simple = shuffle(["Bump", "Nudge", "Spatula", "Spanner", "Lockpick", "Doppeldice", "Duplicate", "Vise Grip", "Cauldron"]);
+
+var intermediate = shuffle([
+// Other Vanilla
+// Charms
+"Snake Eye Charm", "Square Pair", "Midnight Charm", "Half Moon Charm",
+// Keys
+"Berlin Key", "Master Key", "Skeleton Key", "Magic Key",
+// Dice Storage
+"Illuminate", "Paper Lantern", "Picnic Basket", "Marmalade",
+// Cauldrons
+"Glass Cauldron", "Silver Cauldron", "Gold Cauldron", "Blood Cauldron", "Crucible",
+// Fury and Reuse
+"Woof Woof Woof", "Chocolate Cookie", "Hookshot", "Fury Spell",
+// Misc
+"Counterfeit", "Hacksaw", "Chisel", "Sine Wave", "Saw Wave", "Doppeltwice", "Lollipop", 
+"Subdivide", "Smush Together", "Rat", "Change Machine", "Ungeradedice", "Signal Jammer",
+"Honey", "Lucky Roll",
+
+// Vanillaish
+"Acorn Shield", "Pirate Hookshot", "Kitchen Tongs", "Allen Key", "Rubber Duck",
+
+// Modded
+"Dime", "Parity Flip", "Shrug", "5patula", "Rerow", "Scrambler", "Natural Charm",
+"Caustic Cauldron", "Cactus Cauldron", "Halfula", "Radicalise", "Waterwheel", "Murk",
+"Burner Phone", "Demote", "Locktrick", "Mitosis", "Janky Spanner", "Rock Crusher",
+"Decantation", "Permapick"
+
+// Note to self, add reunion items
+]);
+
+var advanced = shuffle([
+// Vanilla
+"Befuddle", "Blender", "Table Slam", "Crowbar", "Bearpick", "Giant Spatula", "Magic Reroll", 
+"Candle", "Avalanche", "Splitula",
+
+// Vanillaish
+"Autotune", "Bicep Curl", "Bell Peppers",
+
+// Modded
+"Dupliduddle", "Eternal Cauldron", "Countvalanche", "Star Cauldron", "Dice Pump", "Wide Spatula",
+"Luckpick", "Dullplicate", "One I Prepared Earlier", "Half Moon Cauldron", "Buzzsaw", "Bumper Sticker",
+"Trinity Charm", "Rockfall", "Metal Crusher", "Plug Socket", "Chain Link", "God's Pick", "Sharpie",
+"Transporter"
+]);
+
+var exotic = shuffle(["Lucky Star", "Disco Ball", "Logistics Expert", "Call for Backup Robot?", "Mass Grimoire", "Obsidian Bauble", "Sugar Rush"]);
+
+var spare = shuffle(["Spare 1", "Spare 2","Spare 3","Spare 4","Spare 5","Spare 6", "Wizard's Hat"]);
+
+//var awesomelist = shuffle(["Bronze Cauldron", "Mirror Cauldron", "Broken Mirror", "Hall of Mirrors", "Mirror Spatula", "Endless Pond", "Fogged Mirror", "Deva Form"]);
 
 var music = shuffle(['xmas1', 'xmas2', 'xmas3', 'xmas10', 'xmas6']);
 
@@ -13,7 +64,7 @@ var goodotherstuff = [];
 
 //Floor 1:
 items = [];
-gooditems = [awesomelist.pop()];
+gooditems = [simple.pop()];
 otherstuff = [];
 goodotherstuff = [];
 
@@ -22,10 +73,10 @@ mycoolfloor1.theme = rand([music.pop()]);
 mycoolfloor1.generate();
 
 //Floor 2:
-items = [];
-gooditems = [awesomelist.pop()];
+items = [intermediate.pop(), simple.pop()];
+gooditems = [];
 otherstuff = [health()];
-goodotherstuff = [shop([awesomelist.pop(), awesomelist.pop(), awesomelist.pop()])];
+goodotherstuff = [shop([simple.pop(), intermediate.pop(), intermediate.pop()])];
 
 
 var mycoolfloor2 = addfloor('small').additems(items, gooditems).addotherstuff(otherstuff, goodotherstuff);
@@ -33,14 +84,13 @@ mycoolfloor2.theme = rand([music.pop()]);
 mycoolfloor2.generate();
 
 //Floor 3:
-items = [];
-items.push(awesomelist.pop());
+items = [intermediate.pop(), exotic.pop()];
 gooditems = [];
 
 otherstuff = [health(), health()];
 
 goodotherstuff = [
-  shop([awesomelist.pop(), awesomelist.pop(), awesomelist.pop()]),
+  shop([spare.pop(), intermediate.pop(), advanced.pop()], [4, 2, 3]),
   upgrade()
 ];
 
@@ -49,12 +99,12 @@ mycoolfloor3.theme = rand([music.pop()]);
 mycoolfloor3.generate();
   
 //Floor 4:
-items = [];
-gooditems = [awesomelist.pop()];
+items = [advanced.pop()];
+gooditems = [];
 
 otherstuff = [health()];
 goodotherstuff = [
-  trade(["any"], [awesomelist.pop()])
+  trade(["any"], [advanced.pop()])
 ];
 
 var mycoolfloor4 = addfloor('big').additems(items, gooditems).addotherstuff(otherstuff, goodotherstuff);
@@ -62,14 +112,13 @@ mycoolfloor4.theme = rand([music.pop()]);
 mycoolfloor4.generate();
   
 //Floor 5:
-items = [];
-items.push(awesomelist.pop());
+items = [advanced.pop()];
 gooditems = [];
 
 otherstuff = [health(), health()];
 goodotherstuff = [
   upgrade(),
-  shop(["health", strangeshop.pop(), "upgrade"], [4, 4, 4])
+  shop(["health", exotic.pop(), "upgrade"], [4, 4, 4])
 ];
 
 var mycoolfloor5 = addfloor('big').additems(items, gooditems).addotherstuff(otherstuff, goodotherstuff);
