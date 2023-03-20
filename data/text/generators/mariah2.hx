e6bc2cf0f7a7357c56cdeaa7f8b46ada27d26e05
That-One-Dude <tony.skeps@gmail.com>
2023-03-19 22:25:47 -0500

    Added more equipment
    Added Skippidy Bop|Do Wop
    Added Mistletoe|Holly Spikes
    Added Black Friday|Cyber Monday
    Added Christmas Spirit|Joy of Giving
    
    Decided on the starting set of cards
    Added a Jester like Level 2 deck chosen decides run thing

---
+++ data/text/generators/mariah2.hx
@@ -0,0 +1,91 @@
+// Items:
+// Spiked Sword|Plated Shield, 365 Days of Christmas|Burnt Out, Gift To Me|Gift To You,
+// On The Flipside|On The Flipside, Red Stripes|White Stripes, Spanning Lockpick|Lockpicking Spanner
+// Nudging Bump|Bumping Nudge, Doppeling Duplicate|Duplicating Doppeldice, Verse|Chorus
+// Fit of Rage|Chill Out Boomerang Throw|Boomerang Catch, Skippity Bop|Do Wop
+// Mistletoe|Holly Spikes, Black Friday|Cyber Monday, Christmas Spirit|Joy of Giving
+
+var notes = shuffle(["Dark Song","Healthy Song","Holding The Note","Emotions","Opera","Another Song"]);
+var latenotes = ["Duet","Mic Check","Pitch Poison","Killer Karaoke"];
+var finale = ["Curtain Call"];
+var vampireitem = ["Silver Sword"];
+
+var itempools = [notes, latenotes, vampireitem];
+
+runscript("christmasspecial/checkgeneratorisaccurate",[itempools]);
+
+var items = [];
+var gooditems = [];
+var otherstuff = [];
+var goodotherstuff = [];
+
+usestandardenemies();
+
+//Floor 1:
+items = [notes.pop()];
+gooditems = [];
+otherstuff = [];
+goodotherstuff = [];
+var mycoolfloor1 = addfloor('tiny').additems(items, gooditems);
+mycoolfloor1.theme = rand(['music_mariahlow']);
+mycoolfloor1.setlocation("GAMESHOW");
+mycoolfloor1.generate();
+
+//Floor 2:
+items = [notes.pop()];
+gooditems = [finale.pop()];
+otherstuff = [health()];
+goodotherstuff = [];
+var mycoolfloor2 = addfloor('big').additems(items, gooditems).addotherstuff(otherstuff, goodotherstuff);
+mycoolfloor2.theme = mycoolfloor1.theme;
+mycoolfloor2.setlocation("GAMESHOW");
+mycoolfloor2.generate();
+
+//Floor 3:
+items = [notes.pop()];
+gooditems = [latenotes.pop()];
+otherstuff = [health()];
+
+goodotherstuff = [
+  shop(["health", "upgrade", "health"], [4, 4, 4])
+];
+var mycoolfloor3 = addfloor('big').additems(items, gooditems).addotherstuff(otherstuff, goodotherstuff);
+mycoolfloor3.theme = rand(['music_mariahlow']);
+mycoolfloor3.setlocation("GAMESHOW");
+mycoolfloor3.generate();
+
+//Floor 4:
+items = [latenotes.pop()];
+gooditems = [];
+
+otherstuff = [health()];
+goodotherstuff = [];
+var mycoolfloor4 = addfloor('big').additems(items, gooditems).addotherstuff(otherstuff, goodotherstuff);
+mycoolfloor4.theme = rand(['music_mariahhigh']);
+mycoolfloor4.setlocation("GAMESHOW");
+mycoolfloor4.generate();
+  
+//Floor 5:
+items = [latenotes.pop()];
+gooditems = [];
+
+otherstuff = [health()];
+goodotherstuff = [
+  shop(["health", "health", "upgrade"], [4, 4, 4])
+];
+
+var mycoolfloor5 = addfloor('big').additems(items, gooditems).addotherstuff(otherstuff, goodotherstuff);
+mycoolfloor5.theme = rand(['music_mariahhigh']);
+mycoolfloor5.setlocation("GAMESHOW");
+mycoolfloor5.generate();
+
+// Floor 6:            
+items = [];
+gooditems = [];
+otherstuff = [];
+goodotherstuff = [];
+
+var mycoolfloor6 = addfloor('boss').additems(items, gooditems).addotherstuff(otherstuff, goodotherstuff);
+mycoolfloor6.theme = rand(['music_mariahboss']);
+mycoolfloor6.setlocation("boss");
+mycoolfloor6.generate();
