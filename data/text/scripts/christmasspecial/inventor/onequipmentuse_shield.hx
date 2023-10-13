329e8c56405dfaf11b12ab7e40dc9bc580813c1f
EdX7 <edluo1@bu.edu>
2023-10-13 00:50:19 -0700

    still buggy, but added the shield for equipment

--- data/text/scripts/christmasspecial/inventor/onequipmentuse_shield.hx
+++ data/text/scripts/christmasspecial/inventor/onequipmentuse_shield.hx
@@ -1,6 +1,16 @@
+// Code by Ed x 7
+
 trace("TEST EQUIPMENT USE");
 
 var fighter = args[0];
 var equipment = args[1];
+
+// Identify the equipment used, and the top-right value on the equipment.
+
+// Remove the former status.
+fighter.removestatus("inventor_shield");
+
+// Add the new status.
+fighter.addstatus("inventor_shield", equipment.totalusesremaining);
 
 trace(equipment);
