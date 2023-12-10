6b7902dbf1890b1885023dc51ce905b1b89f7df4
That-One-Dude <tony.skeps@gmail.com>
2023-12-10 17:52:34 -0600

    Finished Mariah, barring equipment and overall generator design

--- data/text/generators/mariah2.hx
+++ data/text/generators/mariah2.hx
@@ -2,17 +2,19 @@
 // Spiked Sword|Plated Shield, 365 Days of Christmas|Burnt Out, Gift To Me|Gift To You,
 // On The Flipside|On The Flipside, Red Stripes|White Stripes, Spanning Lockpick|Lockpicking Spanner
 // Nudging Bump|Bumping Nudge, Doppeling Duplicate|Duplicating Doppeldice, Verse|Chorus
-// Fit of Rage|Chill Out Boomerang Throw|Boomerang Catch, Skippity Bop|Do Wop
+// Fit of Rage|Chill Out, Boomerang Throw|Boomerang Catch, Skippity Bop|Do Wop
 // Mistletoe|Holly Spikes, Black Friday|Cyber Monday, Christmas Spirit|Joy of Giving
+// Cookies|Milk
+// Peaceful Strum|Angry Strum, Copyright|Free Use, Touch My Body|Don't Touch My Body
+// Obsessed|It's A Wrap, Constant Note|Constant Note
 
-var notes = shuffle(["Dark Song","Healthy Song","Holding The Note","Emotions","Opera","Another Song"]);
-var latenotes = ["Duet","Mic Check","Pitch Poison","Killer Karaoke"];
-var finale = ["Curtain Call"];
 var vampireitem = ["Silver Sword"];
 
-var itempools = [notes, latenotes, vampireitem];
-
-runscript("christmasspecial/checkgeneratorisaccurate",[itempools]);
+var basicItems = ["Red Stripes", "White Stripes", "Spiked Sword", "Plated Shield", "Boomerang Throw", "Cookies", "Milk", "Touch My Body", "Don't Touch My Body", "Obsessed", "It's A Wrap"];
+var diceItems = ["Spanning Lockpick","Lockpicking Spanner", "Nudging Bump", "Bumping Nudge", "Doppeling Duplicate", "Duplicating Doppeldice"];
+var uniqueItems = ["365 Days of Christmas","Gift To Me", "On The Flipside", "Fit of Rage", "Copyright", "Free Use"];
+var noteItems = ["Peaceful Strum", "Angry Strum", "Verse", "Chorus", "Constant Note"];
+var manaItems = ["Mistletoe", "Holly Spikes", "Black Friday", "Cyber Monday", "Christmas Spirit", "Joy of Giving"];
 
 var items = [];
 var gooditems = [];
@@ -22,7 +24,7 @@ var goodotherstuff = [];
 usestandardenemies();
 
 //Floor 1:
-items = [notes.pop()];
+items = [diceItems.pop()];
 gooditems = [];
 otherstuff = [];
 goodotherstuff = [];
@@ -32,22 +34,24 @@ mycoolfloor1.setlocation("GAMESHOW");
 mycoolfloor1.generate();
 
 //Floor 2:
-items = [notes.pop()];
-gooditems = [finale.pop()];
-otherstuff = [health()];
-goodotherstuff = [];
+items = [basicItems.pop()];
+gooditems = [noteItems.pop()];
+otherstuff = [health(), health()];
+goodotherstuff = [
+  shop([uniqueItems.pop(), noteItems.pop(), manaItems.pop()], [3, 2, 2])
+];
 var mycoolfloor2 = addfloor('big').additems(items, gooditems).addotherstuff(otherstuff, goodotherstuff);
 mycoolfloor2.theme = mycoolfloor1.theme;
 mycoolfloor2.setlocation("GAMESHOW");
 mycoolfloor2.generate();
 
 //Floor 3:
-items = [notes.pop()];
-gooditems = [latenotes.pop()];
-otherstuff = [health()];
+items = [basicItems.pop()];
+gooditems = [manaItems.pop()];
+otherstuff = [upgrade(), health(), health()];
 
 goodotherstuff = [
-  shop(["health", "upgrade", "health"], [4, 4, 4])
+  shop([uniqueItems.pop(), diceItems.pop(), noteItems.pop()], [3, 2, 2])
 ];
 var mycoolfloor3 = addfloor('big').additems(items, gooditems).addotherstuff(otherstuff, goodotherstuff);
 mycoolfloor3.theme = rand(['music_mariahlow']);
@@ -55,10 +59,10 @@ mycoolfloor3.setlocation("GAMESHOW");
 mycoolfloor3.generate();
 
 //Floor 4:
-items = [latenotes.pop()];
-gooditems = [];
+items = [basicItems.pop()];
+gooditems = [noteItems.pop()];
 
-otherstuff = [health()];
+otherstuff = [upgrade(), health()];
 goodotherstuff = [];
 var mycoolfloor4 = addfloor('big').additems(items, gooditems).addotherstuff(otherstuff, goodotherstuff);
 mycoolfloor4.theme = rand(['music_mariahhigh']);
@@ -66,12 +70,12 @@ mycoolfloor4.setlocation("GAMESHOW");
 mycoolfloor4.generate();
   
 //Floor 5:
-items = [latenotes.pop()];
-gooditems = [];
+items = [basicItems.pop()];
+gooditems = [basicItems.pop()];
 
 otherstuff = [health()];
 goodotherstuff = [
-  shop(["health", "health", "upgrade"], [4, 4, 4])
+  shop([uniqueItems.pop(), manaItems.pop(), "upgrade"], [4, 4, 4])
 ];
 
 var mycoolfloor5 = addfloor('big').additems(items, gooditems).addotherstuff(otherstuff, goodotherstuff);
